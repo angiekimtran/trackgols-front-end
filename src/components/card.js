b7cf37f6333ae15e20a3c3e6b4a305925c8cdb79
Angie Tran <tranangiekim@gmail.com>
2023-08-01 16:19:44 -0600

    Updated card component

--- src/components/card.js
+++ src/components/card.js
@@ -3,74 +3,73 @@ import { deleteCard, updateCard } from "../../api/cards";
 import "./Card.css";
 
 const Card = ({ id, message, refetchCards }) => {
-    const [isEditing, setIsEditing] = useState(false);
-    const [editedMessage, setEditedMessage] = useState(message);
-    const [warning, setWarning] = useState("");
-  
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedMessage, setEditedMessage] = useState(message);
+  const [warning, setWarning] = useState("");
 
-    const onDelete = () => {
-      deleteCard(id).then(() => {
-        refetchCards();
-      });
-    };
-  
-    const handleEdit = () => {
-      setWarning("");
-      setIsEditing(true);
-    };
-  
-    const handleKeyDown = (event) => {
-      if (event.key === "Enter") {
-        handleSave();
-      }
-    };
-  
-    const handleSave = () => {
-      if (editedMessage.trim() === "") {
-        setWarning("Message must not be blank.");
-        return;
-      } else if (editedMessage.length > 40) {
-        setWarning("Max 40 characters.");
-        return;
-      }
-      onUpdateCard(editedMessage, id);
-      setIsEditing(false);
-    };
-  
-    const handleInputChange = (event) => {
-      setEditedMessage(event.target.value);
-    };
-  
-    const onUpdateCard = (message, id) => {
-      updateCard(message, id).then(() => {
-        refetchCards();
-      });
-    };
-  
-    return (
-      <section className="card">
-        {isEditing ? (
-          <>
-            <textarea
-              value={editedMessage}
-              onChange={handleInputChange}
-              onBlur={handleSave}
-              onKeyDown={handleKeyDown}
-              className="editable-text"
-            />
-            <p className="warning-card">{warning}</p>
-          </>
-        ) : (
-          <p className="text" onClick={handleEdit}>
-            {message}
-          </p>
-        )}
-        <button className="delete" onClick={onDelete}>
-          X
-        </button>
-      </section>
-    );
+
+  const onDelete = () => {
+    deleteCard(id).then(() => {
+      refetchCards();
+    });
   };
-  
-  export default Card;
-  
+
+  const handleEdit = () => {
+    setWarning("");
+    setIsEditing(true);
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      handleSave();
+    }
+  };
+
+  const handleSave = () => {
+    if (editedMessage.trim() === "") {
+      setWarning("Message must not be blank.");
+      return;
+    } else if (editedMessage.length > 40) {
+      setWarning("Max 40 characters.");
+      return;
+    }
+    onUpdateCard(editedMessage, id);
+    setIsEditing(false);
+  };
+
+  const handleInputChange = (event) => {
+    setEditedMessage(event.target.value);
+  };
+
+  const onUpdateCard = (message, id) => {
+    updateCard(message, id).then(() => {
+      refetchCards();
+    });
+  };
+
+  return (
+    <section className="card">
+      {isEditing ? (
+        <>
+          <textarea
+            value={editedMessage}
+            onChange={handleInputChange}
+            onBlur={handleSave}
+            onKeyDown={handleKeyDown}
+            className="editable-text"
+          />
+          <p className="warning-card">{warning}</p>
+        </>
+      ) : (
+        <p className="text" onClick={handleEdit}>
+          {message}
+        </p>
+      )}
+      <button className="delete" onClick={onDelete}>
+        X
+      </button>
+    </section>
+  );
+};
+
+export default Card;
