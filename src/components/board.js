77f31905b33bd2745eea9f4508f1ef32a8b686fc
Angie Tran <tranangiekim@gmail.com>
2023-08-04 00:50:25 -0600

    got data visible in web app

--- src/components/board.js
+++ src/components/board.js
@@ -2,12 +2,22 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 import { isEmpty } from 'lodash'
 import { getBoard } from '../api/boards'
+import {getColumns} from '../api/columns'
+import Column from './column'
 
 const Board = () => {
     const [board, setBoard] = useState({})
+    const [columns, setColumns] = useState([])
+
     const fetchBoard = () => {
         getBoard().then((data) => {
             setBoard(data)
+        })
+    }
+
+    const fetchColumns = (id) => {
+        getColumns(id).then((data) => {
+            setColumns(data)
         })
     }
 
@@ -17,7 +27,29 @@ const Board = () => {
         }
     })
 
-    return <div>{board?.title}</div>
+    useEffect(() => {
+        if (!isEmpty(board) && board?._id) {
+            fetchColumns(board._id)
+        }
+    }, [board])
+
+    return (
+        <section>
+            <div>{board?.title}</div>
+            <div>
+                <ul>
+                    {columns && columns.map((column) => 
+                        <Column
+                        key={column._id}
+                        id={column._id}
+                        title={column.title}
+                        fetchColumns={fetchColumns}
+                        />
+                    )}
+                </ul>
+            </div>
+        </section>
+    )
 }
 
 export default Board
--- src/components/column.js
+++ src/components/column.js
@@ -1,62 +1,41 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
-// import { useDrag } from 'react-dnd'
 
 import Card from './card'
 import { getCards } from '../api/cards'
-import { getColumn } from '../api/columns'
+// import { getColumn } from '../api/columns'
 import './column.css'
 
-const Column = ({ columnID, onDragCard }) => {
-    const [cards, setCards] = useState({})
-    const [column, setColumn] = useState({})
+const Column = ({ id, title, fetchColumns}) => {
+    const [cards, setCards] = useState([])
 
-    // const onSelect = (id) => {
-    //   onDragCard(id);
-    // };
-
-    const fetchColumn = async () => {
-        const selectedColumn = await getColumn(columnID)
-        setColumn(selectedColumn)
-    }
-
-    const refetchCards = async () => {
-        const retrieveColumns = await getCards(columnID)
-        setCards(retrieveColumns)
+    const fetchCards = (id) => {
+        getCards(id).then((data) => {
+            setCards(data)
+        })
     }
 
     useEffect(() => {
-        if (columnID) {
-            fetchColumn()
-            refetchCards()
+        if (id) {
+            fetchCards(id)
         }
-    })
-
+    }, [id])
+    console.log(cards)
     return (
         <section className="column">
-            <h2 className="column-title">{column?.title}</h2>
+            <h2 className="column-title">{title}</h2>
             <section className="card-list">
                 <ul>
                     {cards &&
                         cards.map((card) => (
                             <Card
-                                key={card.card_id}
-                                id={card.card_id}
+                                key={card._id}
+                                id={card._id}
                                 message={card.message}
-                                refetchCards={refetchCards}
+                                fetchCards={fetchCards}
                             />
                         ))}
                 </ul>
-                {/* <ul>
-            {cards &&
-            cards.map((card) => (
-                <li key={card.card_id}>
-                <button onClick={() => onSelect(card.card_id)}>
-                    {card.message}
-                </button>
-                </li>
-            ))}
-        </ul> */}
             </section>
         </section>
     )
